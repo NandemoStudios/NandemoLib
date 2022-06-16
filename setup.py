3f82e29fd8ac2be6db5057fca2e64eb6f755f749
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2022-06-16 11:03:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import requests
 
 file = requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/AutoUpdate.py')
+file2 = requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/main.py')
 
 Au = open("AutoUpdate.py",'w+')
 Au.write(file.text)
+Au.close()
+
+Au = open("main.py",'w+')
+Au.write(file2.text)
 Au.close()
