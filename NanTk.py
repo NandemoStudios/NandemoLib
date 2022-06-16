8b0ae04bef288d8f03a0fc6c37e3d05562dba8c8
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2022-06-16 13:05:21 +0100

    Update NanTk.py

--- NanTk.py
+++ NanTk.py
@@ -1,1 +1,12 @@
-from tkinteri import *
+from tkinter import *
+import requests
+
+if requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/NanTk.py') == open('NanTk.py','r').read():
+  pass
+else:
+  f = requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/NanTk.py')
+  ftext = f.text
+  file = open('NanTk.py','w')
+  file.write(ftext)
+  file.close()
+  
