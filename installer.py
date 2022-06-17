79744a8928f700fa67bc1ad44168c790f7dba557
Swyftl <68863847+NandemoStudios@users.noreply.github.com>
2022-06-17 09:58:48 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,1 +1,6 @@
+import requests
 
+NanTk = requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/NanTk.py')
+quiz = requests.get('https://raw.githubusercontent.com/NandemoStudios/NandemoLib/main/game/quiz.py')
+
+installing = ['False', 'False']
